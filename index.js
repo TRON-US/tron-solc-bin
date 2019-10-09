364d4c3fde6e257d8596de44746092d812792b2b
Francesco Sullo <sullof@gmail.com>
2019-10-09 09:50:41 -0700

    Cleaner version

--- index.js
+++
@@ -1,6 +0,0 @@
-
-module.exports.v0_4_24 = require('./bin/0.4.24')
-module.exports.v0_4_25 = require('./bin/0.4.25')
-module.exports.v0_5_4 = require('./bin/0.5.4')
-module.exports.v0_5_8 = require('./bin/0.5.8')
-module.exports.v0_5_9 = require('./bin/0.5.9')
