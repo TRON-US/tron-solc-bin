02e43c735f4b80b41cc1c79f20c9f828b60111b3
Francesco Sullo <sullof@gmail.com>
2019-10-09 09:20:10 -0700

    Added the compilers

---
+++ index.js
@@ -0,0 +1,6 @@
+
+module.exports.v0_4_24 = require('./versions/0.4.24')
+module.exports.v0_4_25 = require('./versions/0.4.25')
+module.exports.v0_5_4 = require('./versions/0.5.4')
+module.exports.v0_5_8 = require('./versions/0.5.8')
+module.exports.v0_5_9 = require('./versions/0.5.9')
